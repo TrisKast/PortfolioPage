3981e5115873c55902f436da9b65cfc6a101c791
TrisKast <kast@mpusp.mpg.de>
2020-10-19 15:22:44 +0200

    Wrote some text for the about-me section; Added media query

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 window.addEventListener("scroll", checkOffSet);
 
+
 const navbar = document.querySelector('#navbar')
 
 function checkOffSet(e){
-    if(window.pageYOffset > 0){
-        navbar.style.backgroundColor = "white";
-        navbar.style.padding = "1%";
-    }
-    else{
-        navbar.style.backgroundColor = "transparent";
-        navbar.style.padding = "1%";
-    }
+    // if(window.pageYOffset > 0){
+    //     navbar.style.backgroundColor = "white";
+    //     navbar.style.padding = "1%";
+    // }
+    // else{
+    //     navbar.style.backgroundColor = "transparent";
+    //     navbar.style.padding = "1%";
+    // }
 }
+
