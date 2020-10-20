4d3fd557aae002270bf81870f10ba9e9f646b2de
TrisKast <kast@mpusp.mpg.de>
2020-10-20 08:41:35 +0200

    Started working on the contact section

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ window.addEventListener("scroll", checkOffSet);
 const navbar = document.querySelector('#navbar')
 
 function checkOffSet(e){
-    // if(window.pageYOffset > 0){
-    //     navbar.style.backgroundColor = "white";
-    //     navbar.style.padding = "1%";
-    // }
-    // else{
-    //     navbar.style.backgroundColor = "transparent";
-    //     navbar.style.padding = "1%";
-    // }
+    if(window.pageYOffset > 0){
+        navbar.style.backgroundColor = "white";
+        navbar.style.padding = "1%";
+        navbar.style.borderBottom = "thin solid black";
+    }
+    else{
+        navbar.style.backgroundColor = "transparent";
+        navbar.style.padding = "1%";
+        navbar.style.borderBottom = "none";
+    }
 }
 
