3c2b13f056ab4d59c65c7a88a032585bb82e37c0
TrisKast <kast@mpusp.mpg.de>
2020-10-24 23:26:11 +0200

    Worked on the charts in the skill section

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-window.addEventListener("scroll", checkOffSet);
-
-
 const navbar = document.querySelector('#navbar')
-
 const slideButtonLeft = document.querySelector('#slide-button-left');
 const slideButtonRight = document.querySelector('#slide-button-right');
 const welcomeText = document.querySelector('#welcome-text')
 const resume = document.querySelector('#resume')
 const skills = document.querySelector('#skills')
 const personal = document.querySelector('#personal')
+const chartButtons = Array.from(document.querySelectorAll('.chart-button'));
+const resume_stations = Array.from(document.querySelectorAll('.time-line-station'))
+const stationName = document.querySelector('#station-name')
+const stationDuration = document.querySelector('#station-duration')
+const stationTechnologies = document.querySelector('#station-technologies')
+const stationDescription = document.querySelector('#station-description')
 
 resume.style.display='none' 
-skills.style.display='none' 
 personal.style.display='none' 
-
 let currPersonalPos = 'text'
 
+window.addEventListener("scroll", checkOffSet);
+chartButtons.forEach(button => button.addEventListener('click', changeChart));
 slideButtonLeft.addEventListener('click', slide_left)
 slideButtonRight.addEventListener('click', slide_right)
+resume_stations.forEach(station => station.addEventListener('click', activte_resume_station, false))
+resume_stations.forEach(station => station.addEventListener('mouseenter', setHiglight, false))
+resume_stations.forEach(station => station.addEventListener('mouseleave', revertHighlight, false))
 
 function slide_right(){
 
@@ -27,18 +32,48 @@ function slide_right(){
         currPersonalPos = 'resume'
     }else if(currPersonalPos == 'resume'){
         resume.style.display='none' 
-        skills.style.display='block' 
-        currPersonalPos = 'skills'
-    } else if(currPersonalPos == 'skills'){
-        skills.style.display='none' 
         personal.style.display='block' 
         currPersonalPos = 'personal'
     } else if(currPersonalPos == 'personal'){
         personal.style.display='none' 
         welcomeText.style.display='block' 
         currPersonalPos = 'text'
+    } 
+}
+
+function changeChart(e){
+    chartButtons.forEach(button => button.classList.add('is-outlined'))
+    document.querySelector('#'+e.target.id).classList.remove('is-outlined')
+
+    if(e.target.id == 'chart-button-1'){
+        zingchart.render({
+            id: 'skillChart',
+            data: webDevChartConfig,
+            width: '100%',
+            height: '100%',
+          });
+    } else if(e.target.id == 'chart-button-2'){
+        zingchart.render({
+            id: 'skillChart',
+            data: dataAnalysisChartConfig,
+            width: '100%',
+            height: '100%',
+          });
+    } else if(e.target.id == 'chart-button-3'){
+        zingchart.render({
+            id: 'skillChart',
+            data: generalChartConfig,
+            width: '100%',
+            height: '100%',
+          });
+    } else if(e.target.id == 'chart-button-4'){
+        zingchart.render({
+            id: 'skillChart',
+            data: usageChart,
+            width: '100%',
+            height: '100%',
+          });
     }
-    
 }
 
 function slide_left(){
@@ -49,10 +84,6 @@ function slide_left(){
         currPersonalPos = 'personal'
     }else if(currPersonalPos == 'personal'){
         personal.style.display='none' 
-        skills.style.display='block' 
-        currPersonalPos = 'skills'
-    }else if(currPersonalPos == 'skills'){
-        skills.style.display='none' 
         resume.style.display='block' 
         currPersonalPos = 'resume'
     } else if(currPersonalPos == 'resume'){
@@ -75,12 +106,6 @@ function checkOffSet(e){
     }
 }
 
-
-const resume_stations = Array.from(document.querySelectorAll('.time-line-station'))
-resume_stations.forEach(station => station.addEventListener('click', activte_resume_station, false))
-resume_stations.forEach(station => station.addEventListener('mouseenter', setHiglight, false))
-resume_stations.forEach(station => station.addEventListener('mouseleave', revertHighlight, false))
-
 function setHiglight(e){
     e.target.classList.add('station-hovered')
     e.target.childNodes[1].style.color = 'hsl(159, 83%, 27%)'
@@ -91,17 +116,9 @@ function revertHighlight(e){
     e.target.childNodes[1].style.color = 'black'
 }
 
-const stationName = document.querySelector('#station-name')
-const stationDuration = document.querySelector('#station-duration')
-const stationTechnologies = document.querySelector('#station-technologies')
-const stationDescription = document.querySelector('#station-description')
-
 function activte_resume_station(e){
 
-    //resume_stations.forEach(station => station.style.backgroundColor = 'white')
     resume_stations.forEach(station => station.classList.remove('selected-station'))
-
-    //document.querySelector('#station-BSc-wrapper').classList.remove('selected-station')
 
     
     if(e.target.id == 'station-BSc-wrapper' || e.target.id == 'station-BSc'){
@@ -161,7 +178,7 @@ function activte_resume_station(e){
     
 }
 
-var myConfig = {
+var usageChart = {
     "type": "line",
     "utc": true,
     "plotarea": {
@@ -252,24 +269,7 @@ var myConfig = {
     },
     "series": [{
         "values": [
-            0,
-            1,
-            2,
-            2,
-            2,
-            3,
-            3,
-            2,
-            2,
-            2,
-            2,
-            2,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1
+            0,1,2,2,2,3,3,2,2,2,1,1,1,1,1,1,1,1
         ],
         "text": "Java",
         "line-color": "#007790",
@@ -293,26 +293,7 @@ var myConfig = {
         }
       },
       {
-        "values": [
-            0,
-            0,
-            0,
-            0,
-            1,
-            2,
-            2,
-            2,
-            2,
-            2,
-            3,
-            3,
-            4,
-            4,
-            5,
-            5,
-            5,
-            5
-        ],
+        "values": [0,0,0,0,1,2,2,2,2,2,3,3,4,4,5,5,5,5],
         "text": "Python",
         "line-color": "#009872",
         "legend-item": {
@@ -335,26 +316,7 @@ var myConfig = {
         }
       },
       {
-        "values": [
-            0,
-            0,
-            0,
-            0,
-            1,
-            1,
-            2,
-            2,
-            2,
-            3,
-            3,
-            2,
-            2,
-            3,
-            3,
-            2,
-            2,
-            2
-        ],
+        "values": [0,0,0,0,1,1,2,2,2,3,3,2,2,3,3,2,2,2],
         "text": "R",
         "line-color": "#da534d",
         "legend-item": {
@@ -377,26 +339,7 @@ var myConfig = {
         }
       },
       {
-        "values": [
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            1,
-            2,
-            2,
-            2,
-            1,
-            1,
-            1,
-            1,
-            1,
-            2,
-            4,
-            5
-        ],
+        "values": [0,0,0,0,0,0,1,2,2,2,1,1,1,1,1,2,4,5],
         "text": "Javascript",
         "line-color": "rgb(165, 200, 130)",
         "legend-item": {
@@ -419,26 +362,7 @@ var myConfig = {
         }
       },  
       {
-        "values": [
-            0,
-            0,
-            1,
-            2,
-            2,
-            2,
-            1,
-            1,
-            1,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0
-        ],
+        "values": [0,0,1,2,2,2,1,1,1,0,0,0,0,0,0,0,0,0],
         "text": "C++",
         "line-color": "rgb(90, 177, 187)",
         "legend-item": {
@@ -461,26 +385,7 @@ var myConfig = {
         }
       },
       {
-        "values": [
-            0,
-            0,
-            0,
-            0,
-            1,
-            1,
-            1,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2
-        ],
+        "values": [0,0,0,0,1,1,1,2,2,2,2,2,2,2,2,2,2,3],
         "text": "HTML+CSS",
         "line-color": "rgb(90, 177, 187)",
         "legend-item": {
@@ -501,53 +406,261 @@ var myConfig = {
           "size": 6,
           "background-color": "rgb(90, 177, 187)",
         }
-      },
-      {
-        "values": [
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            1,
-            3
-        ],
-        "text": "Vue.js",
-        "line-color": "rgb(90, 177, 187)",
-        "legend-item": {
-          "background-color": "rgb(90, 177, 187)",
-          "borderRadius": 5,
-          "font-color": "white"
-        },
-        "legend-marker": {
-          "visible": false
-        },
-        "marker": {
-          "background-color": "rgb(90, 177, 187)",
-          "border-width": 1,
-          "shadow": 0,
-          "border-color": "#faa39f"
-        },
-        "highlight-marker": {
-          "size": 6,
-          "background-color": "rgb(90, 177, 187)",
-        }
-      },
+      }
     ]
+  };
+
+let webDevChartData = [{
+    id: 'WebDevelopment',
+    text: 'WebDevelopment',
+    parent: ''
+  }, {
+    id: 'front-end',
+    text: 'front-end',
+    parent: 'WebDevelopment'
+  }, {
+    id: 'css',
+    text: 'css',
+    parent: 'front-end'
+  }, {
+    id: 'Bnotstrap4',
+    text: 'bootstrap4',
+    parent: 'css',
+    value: 1
+  }, {
+    id: 'bulma',
+    text: 'bulma',
+    parent: 'css',
+    value: 1
+  }, {
+    id: 'SASS',
+    text: 'SASS',
+    parent: 'css',
+    value: 1
+  }, {
+    id: 'javascript',
+    text: 'javascript',
+    parent: 'front-end'
+  }, {
+    id: 'ES6',
+    text: 'ES6',
+    parent: 'javascript',
+    value: 1
+  }, {
+    id: 'typescript',
+    text: 'typescript',
+    parent: 'javascript',
+    value: 1
+  }, {
+    id: 'vuejs',
+    text: 'Vue.js',
+    parent: 'javascript',
+    value: 1
+  }, {
+    id: 'html',
+    text: 'html',
+    parent: 'front-end',
+    value: 1
+  }, {
+    id: 'back-end',
+    text: 'back-end',
+    parent: 'WebDevelopment'
+  },  {
+    id: 'frameworks',
+    text: 'frameworks',
+    parent: 'WebDevelopment'
+  },   {
+    id: 'django',
+    text: 'django',
+    parent: 'frameworks',
+    value: 1
+  },   {
+    id: 'flask',
+    text: 'flask',
+    parent: 'frameworks',
+    value: 1
+  }, {
+    id: 'databases',
+    text: 'databases',
+    parent: 'back-end'
+  }, {
+    id: 'MongoDB',
+    text: 'MongoDB',
+    parent: 'databases',
+    value: 1
+  }, {
+    id: 'PostGresSQL',
+    text: 'PostGresSQL',
+    parent: 'databases',
+    value: 1
+  }, {
+    id: 'python',
+    text: 'python',
+    parent: 'back-end',
+    value: 1
+  }];
+
+let webDevChartConfig = {
+    type: 'sunburst',
+    plot: {
+        tooltip: {
+          align: 'left',
+          padding: '10px 15px',
+          borderRadius: '3px',
+          thousandsSeparator: ','
+        },
+        valueBox: {
+          text: '%data-vbtext',
+          color: '#424242',
+          fontSize: '10px',
+          visible: null
+        },
+        tooltipText: '<span style=font-size:19px>%plot-text</span>',
+      },
+            
+    plotarea: {
+        margin: '5px'
+    },
+    options: {
+        space: 0,
+        slice: 0,
+        palette: ['#7CA82B', '#29A2CC', '#D31E1E', '#EF8535', '#A05F18', '#265E96', '#6B7075', '#28536B', '#A87356', '#70AE6E', '#764493', '#BA163B', '#E8630A']
+    },
+    scaleR: {
+        RefAngle: 180,
+        Aperture: 180
+    },
+    series: webDevChartData
+}
+
+let dataAnalysisChartData = [{
+    id: 'data-analysis',
+    text: 'data-analysis',
+    parent: ''
+  }, {
+    id: 'front-end',
+    text: 'front-end',
+    parent: 'data-analysis'
+  },{
+    id: 'back-end',
+    text: 'back-end',
+    parent: 'data-analysis'
+  },  {
+    id: 'frameworks',
+    text: 'frameworks',
+    parent: 'data-analysis'
+  }];
+
+  let dataAnalysisChartConfig = {
+    type: 'sunburst',
+    plot: {
+        tooltip: {
+          align: 'left',
+          padding: '10px 15px',
+          borderRadius: '3px',
+          thousandsSeparator: ','
+        },
+        valueBox: {
+          text: '%data-vbtext',
+          color: '#424242',
+          fontSize: '10px',
+          visible: null
+        },
+        tooltipText: '<span style=font-size:19px>%plot-text</span>',
+      },
+            
+    plotarea: {
+        margin: '5px'
+    },
+    options: {
+        space: 0,
+        slice: 0,
+        palette: ['#7CA82B', '#29A2CC', '#D31E1E', '#EF8535', '#A05F18', '#265E96', '#6B7075', '#28536B', '#A87356', '#70AE6E', '#764493', '#BA163B', '#E8630A']
+    },
+    scaleR: {
+        RefAngle: 180,
+        Aperture: 180
+    },
+    series: dataAnalysisChartData
+}
+
+let generalChartData = [{
+    id: 'data-analysis',
+    text: 'data-analysis',
+    parent: ''
+  }, {
+    id: 'front-end',
+    text: 'front-end',
+    parent: 'data-analysis'
+  },{
+    id: 'back-end',
+    text: 'back-end',
+    parent: 'data-analysis'
+  },  {
+    id: 'frameworks',
+    text: 'frameworks',
+    parent: 'data-analysis'
+  }];
+
+  let generalChartConfig = {
+    type: 'sunburst',
+    plot: {
+        tooltip: {
+          align: 'left',
+          padding: '10px 15px',
+          borderRadius: '3px',
+          thousandsSeparator: ','
+        },
+        valueBox: {
+          text: '%data-vbtext',
+          color: '#424242',
+          fontSize: '10px',
+          visible: null
+        },
+        tooltipText: '<span style=font-size:19px>%plot-text</span>',
+      },
+            
+    plotarea: {
+        margin: '5px'
+    },
+    options: {
+        space: 0,
+        slice: 0,
+        palette: ['#7CA82B', '#29A2CC', '#D31E1E', '#EF8535', '#A05F18', '#265E96', '#6B7075', '#28536B', '#A87356', '#70AE6E', '#764493', '#BA163B', '#E8630A']
+    },
+    scaleR: {
+        RefAngle: 180,
+        Aperture: 180
+    },
+    series: generalChartData
+}
+
+let skillCloudConfig = {
+    type: 'wordcloud',
+    options: {
+      text: 'Python Javascript Java C++ HTML CSS SASS Vue.js Bulma Bootstrap MongoDB PostgreSQL Typescript ES6 Django Flask Linux MacOS Git GitHub BitBucket Bash',
+      aspect: 'spiral',
+      colorType: 'palette',
+      ignore: ['establish', 'this'],
+      maxItems: 50,
+      minLength: '3px',
+      palette: ['#D32F2F', '#1976D2', '#9E9E9E', '#E53935', '#1E88E5', '#7E57C2', '#F44336', '#2196F3', '#A1887F'],
+      rotate: true,
+      style: {
+        hoverState: {
+          backgroundColor: '#1976D2',
+          borderColor: 'none',
+          borderRadius: '3px',
+          fontColor: 'white'
+        }
+      }
+    }
   };
    
   zingchart.render({
-    id: 'myChart',
-    data: myConfig,
+    id: 'skillChart',
+    data: skillCloudConfig,
+    width: '100%',
+    height: '100%',
   });
+
